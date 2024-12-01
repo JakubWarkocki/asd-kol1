97e6509710f8c5910647349f79b350102d13b429
kuba <jakub.warkocki@gmail.com>
2024-12-01 22:34:30 +0100

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### asd-kol1 algorytmy + sprawdzarka (WIP)
+#### Kompilacja
+W repo:
+```bash
+mkdir build
+cd build
+cmake build ..
+make
+./asd-kol1
+```
+Konfuguracja testów w src/main.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 
 int main(){
     List ll = *(new List());
+
+    for(int i =0; i<10000; i++){
+        if(!Tester::testPriorityQueue(ll, i)){
+            Tester::testPriorityQueue(ll, i, true, "LIST");
+        }
+    }
     Tester::testPriorityQueue(ll, 2137, true, "LIST");
+    
 }
